47c2ecba78441ed5c9ecb6a88b7a0180ee1f9cb2
liawiss <vincentliawisk@gmail.com>
2026-03-16 18:31:32 +0700

    Update greeting message in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello Vincent! Proyek CD Berhasil Terdeploy</h1>');
+  res.end('<h1>Proyek CD Berhasil Terdeploy</h1>');
 });
 
 server.listen(port, () => {
